679de10fe5cc444546cbd14b2ffccdc9918d4e9c
Chris Deely <chris@webdeely.com>
2012-05-31 00:25:51 -0400

    modifying background.js to load pre-url settings

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
-var desiredHeaders = [
-    {
-        name:"Access-Control-Allow-Origin",
-        value:"*"
-    },
-    {
-        name: "Access-Control-Allow-Headers",
-        value: "token"
-    }
-];
 /**
  * Returns the index of a given header object in the provided array
  * @param headerArray The Array to search in
@@ -18,7 +8,7 @@ function getHeaderIndex(headerArray, newHeader) {
 
     for (var i = 0, len = headerArray.length; i < len; i++) {
         var currentHeader = headerArray[i];
-        if (currentHeader.hasOwnProperty('name') && currentHeader.name == headerArray.name) {
+        if (currentHeader.hasOwnProperty('name') && currentHeader.name == newHeader.name) {
             return i;
         }
     }
@@ -44,19 +34,52 @@ function mergeNewHeaders(originalHeaders, newHeaders) {
     return mergedHeaders;
 }
 
+/**
+ * Retrieves the saved settings from localstorage
+ * @return an Array of Objects containing { URL:String, headers:[ {name:String, value:String} ] }
+ */
+function retrieveSettings() {
+    var siteSettings = localStorage['corsSites'];
+
+    if (!siteSettings) {
+        return null;
+    }
+
+    return JSON.parse(siteSettings);
+}
+
+function matchUrlToHeaders(url, headersPerUrl) {
+    return [];
+}
+
+var settings = retrieveSettings(),
+    headersPerUrl = {},
+    urlsToAlter = [];
+
+if(settings) {
+    for(var l = settings.length, i=0; i<l; i++) {
+        //push each URL we wish to watch for into the array
+        urlsToAlter.push(settings[i].URL);
+
+        //use the URL as a key in the dictionary to lookup the specific headers to manipulate for that URL
+        headersPerUrl[ settings[i].URL ] = settings[i].headers;
+    }
+}
+
+
+
 chrome.webRequest.onHeadersReceived.addListener(
     function (info) {
 
+        var desiredHeaders = matchUrlToHeaders(info.url, headersPerUrl);
 
         return { responseHeaders:mergeNewHeaders(info.responseHeaders, desiredHeaders) };
 
     },
     // filters
     {
-        urls:[
-            "http://*/*",
-            "https://*/*"
-        ]
+        urls:urlsToAlter
     },
     // extraInfoSpec
-    ["blocking", "responseHeaders"]);
+    ["blocking", "responseHeaders"]
+);
